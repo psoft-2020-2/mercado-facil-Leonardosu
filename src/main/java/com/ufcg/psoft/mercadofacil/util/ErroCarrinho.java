ee84a01d0f4fe082ba9665cf0322df3ea2afc016
Leonardo Su <leonardosu0@gmail.com>
2021-07-25 22:26:40 -0300

    Implementação de Carrinho
    Existem 1 carrinho. Onde é possível:
    1)Adicionar uma quantidade de produto já cadastrado, em estoque e disponível.
    2)Remover uma quantidade de  produto que já foi adicionado no carrinho.
    3)Descartar todo o carrinho, assim apagando todos os produtos que ele continha.
    4)Realizar uma compra, onde compra-se todos os produtos adicionados e gerando uma relatório com detalhes.
    5)Consultar o carrinho, visualizando os produtos já inseridos.

---
+++ src/main/java/com/ufcg/psoft/mercadofacil/util/ErroCarrinho.java
@@ -0,0 +1,31 @@
+package com.ufcg.psoft.mercadofacil.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.ufcg.psoft.mercadofacil.model.Produto;
+
+public class ErroCarrinho {
+
+    static final String CARRINHO_VAZIO = "O carrinho esta vazio";
+    static final String PRODUTO_INVALIDO = "O produto %s nao esta no carrinho";
+    static final String ESTOQUE_INSUFICIENTE = "Nao ha estoque o suficiente do produto %s";
+
+    public static ResponseEntity<CustomErrorType> erroCarrinhoVazio() {
+        return new ResponseEntity<CustomErrorType>(new CustomErrorType(ErroCarrinho.CARRINHO_VAZIO),
+                HttpStatus.NO_CONTENT);
+    }
+
+    public static ResponseEntity<CustomErrorType> erroEstoqueInsuficiente(Produto produto) {
+        return new ResponseEntity<CustomErrorType>(
+                new CustomErrorType(String.format(ErroCarrinho.ESTOQUE_INSUFICIENTE, produto.getNome())),
+                HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    public static ResponseEntity<CustomErrorType> erroProdutoNaoEncontrado(Produto produto) {
+        return new ResponseEntity<CustomErrorType>(
+                new CustomErrorType(String.format(ErroCarrinho.PRODUTO_INVALIDO, produto.getNome())),
+                HttpStatus.NOT_ACCEPTABLE);
+    }
+
+}
